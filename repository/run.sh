67c0bf3dd94106c22bb8c7a28f6a4a8d63c6ab3f
James MacDonald <jmacdoo@gmail.com>
2021-03-16 19:24:11 +0100

    repository will only install if it doesnt exist

--- repository/run.sh
+++ repository/run.sh
@@ -2,9 +2,20 @@
 
 args=""
 
-for var in "${!dl_@}"; do
-    echo "${var:3}" "${!var}"
-    args+=" --${var:3} ${!var}"
-done
+if ! stat /repo/settings/repository.ini
+then
+    echo "installing deadline"
+    for var in "${!dl_@}"; do
+        echo "${var:3}" "${!var}"
+        args+=" --${var:3} ${!var}"
+    done
 
-./DeadlineRepository-*-linux-x64-installer.run ${args}
+    ./DeadlineRepository-*-linux-x64-installer.run ${args}
+
+    echo "deadline install complete"
+
+else
+    echo "skipping deadline install"
+fi
+
+sleep 60s
